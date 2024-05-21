52ad5f9fef1de83a59a39dec6dd1af6fb32a9fde
n0vaesz <hnovaescontato@gmail.com>
2024-05-20 21:52:22 -0300

    connection refused with mongo, reorde ring

--- app.py
+++ app.py
@@ -1,14 +1,32 @@
 from flask import Flask
 from flask_restful import Resource, Api
+from flask_mongoengine import MongoEngine
 
 app = Flask(__name__)
-api = Api(app)
 
+
+app.config['MONGODB_SETTINGS'] = {
+    'db': 'users',
+    'host': 'mongodb',
+    'port': 27017,
+    'username': 'admin',
+    'password': 'admin',
+    'authentication_source': 'admin'
+}
+
+api = Api(app)
+db = MongoEngine(app)
+
+class UserModel(db.Document):
+    cpf = db.StringField(required=True, unique=True)
+    first_name = db.StringField(required=True)
+    last_name = db.StringField(required=True)
+    email = db.EmailField(required=True)
+    birth_date = db.DateTimeField(required=True)
 
 class Users(Resource):
     def get(self):
         return {'message': 'user 1'}
-
 
 class User(Resource):
     def post(self):
@@ -16,7 +34,6 @@ class User(Resource):
 
     def get(self, cpf):
         return {'message': 'CPF'}
-
 
 api.add_resource(Users, '/users')
 api.add_resource(User, '/user', '/user/<string:cpf>')
